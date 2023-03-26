7d642221d31d7d86384dd7c28ca1f9ebeb9096d1
Abimael M <abimaelmercado05@gmail.com>
2023-03-25 22:28:41 -0400

    + favorites toggle

---
+++ Landmarks/Views/Landmakrs/LandmarkList.swift
@@ -0,0 +1,43 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by AM on 3/25/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+    
+    var filteredlandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredlandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+       LandmarkList()
+            .environmentObject(ModelData())
+    }
+}
