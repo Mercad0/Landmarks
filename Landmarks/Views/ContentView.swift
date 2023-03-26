0d5941c8b1c340ade69849bfca79cba3444fb646
Abimael M <abimaelmercado05@gmail.com>
2023-03-25 21:36:02 -0400

    mapping through each landmark

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by AM on 3/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- Landmarks/Views/MapView.swift
+++ Landmarks/Views/MapView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 import MapKit
 
 struct MapView: View {
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D (latitude:  33.881866, longitude: -115.900650),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-        )
-        
-        var body: some View {
-            Map(coordinateRegion: $region)
-        }
-        }
-        
-        struct MapView_Previews: PreviewProvider {
-            static var previews: some View {
-                MapView()
+    var coordinate: CLLocationCoordinate2D
+    @State private var region = MKCoordinateRegion()
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .onAppear {
+                setRegion(coordinate)
             }
-        }
+    }
+    
+    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
+        region = MKCoordinateRegion(
+            center: coordinate, span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(coordinate: CLLocationCoordinate2D (latitude: 33.881866, longitude: -115.900650))
+    }
+}
