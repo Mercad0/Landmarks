4ad1f66a95d828cb730542c88be85a92a9b36930
Abimael M <abimaelmercado05@gmail.com>
2023-03-27 00:09:13 -0400

    add profile with badges settings

---
+++ Landmarks/Views/Profiles/ProfileSummary.swift
@@ -0,0 +1,60 @@
+//
+//  ProfileSummary.swift
+//  Landmarks
+//
+//  Created by AM on 3/26/23.
+//
+
+import SwiftUI
+
+struct ProfileSummary: View {
+    @EnvironmentObject var modelData: ModelData
+    var profile: Profile
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .center, spacing: 10) {
+                Text(profile.username)
+                    .bold()
+                    .font(.title)
+                Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
+                Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
+                Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .center) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                
+                Divider()
+                
+                VStack(alignment: .center) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
+            }
+        }
+    }
+}
+
+struct ProfileSummary_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileSummary(profile: Profile.default)
+            .environmentObject(ModelData())
+    }
+}
